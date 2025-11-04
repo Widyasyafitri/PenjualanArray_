28b2b556fb46baa4faf0aa66c5188e1f4fe36743
Widyasyafitri <widiasafitri9917@gmail.com>
2025-11-05 00:36:54 +0700

    Commit 1 - Setup Awal

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+// penjualanArray.php
+
+// Daftar Produk
+$produk = [
+    ["kode" => "P001", "nama" => "Sprite", "harga" => 10000],
+    ["kode" => "P002", "nama" => "CocaCola B", "harga" => 8000],
+    ["kode" => "P003", "nama" => "Fanta", "harga" => 7000],
+    ["kode" => "P004", "nama" => "Golda", "harga" => 4000],
+    ["kode" => "P005", "nama" => "Aqua", "harga" => 5000],
+];
+
+// Cetak header (tebal dan besar)                                                                                                                                                                       
+echo "<h2><b>--POLGAN MART--</b></h2>";
+echo "Daftar Pembelian<br><br>";
+?>
